2462a0512a4a459eae0ee598cf5739be1d4aa9e4
Bryce Rohloff <126307731+bmrolo@users.noreply.github.com>
2024-06-24 22:38:01 -0400

    added completed script... for now :)

---
+++ quickssh.sh
@@ -0,0 +1,31 @@
+function quickssh() {
+    echo "Where are you trying to go?"
+    echo -n "| d = Most Recent Download | f = Search Downloads | h = Home |"
+    echo
+    read destination
+    if [[ "$destination" == "d" ]]; then
+        checkPath="$HOME/Downloads"
+        ssh_key=$(ls -t $HOME/Downloads/*.pem | head -n 1)
+    elif [[ "$destination" == "f" ]]; then
+        checkPath="$HOME/Downloads"
+        cd $checkPath
+        ssh_key=$(fzf --height=~100% --layout=reverse-list --query ".pem$ !Library")
+    elif [[ "$destination" == "h" ]]; then
+        checkPath="$HOME"
+        cd $HOME
+        ssh_key=$(fzf --height=~100% --layout=reverse-list --query ".pem$ !Library")
+    fi
+    chmod 400 $ssh_key
+    echo -n 'Enter Public IP: '
+    read ec2_ipaddress
+    echo
+    echo -n 'Enter User: (Press Enter for ec2-user) '
+    read vm_user
+    if [ -z "$vm_user" ]; then
+        vm_user="ec2-user"
+    fi
+    echo
+    echo "Using $(basename $ssh_key) as $vm_user to SSH into $ec2_ipaddress..."
+    echo
+    ssh -i $ssh_key $vm_user@$ec2_ipaddress
+    }
