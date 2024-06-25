09cca0556c074c4cdd324bf9e92c56d31c543f65
Bryce Rohloff <126307731+bmrolo@users.noreply.github.com>
2024-06-25 09:45:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,17 +5,19 @@ INSTALL_PATH="/usr/local/bin/quickssh"
 
 # Clone the repository silently
 if ! git clone "$REPO_URL" > /dev/null 2>&1; then
+    echo
     echo "Failed to clone repository."
     exit 1
 fi
 
-cd quickSSH || { echo "Failed to enter directory."; exit 1; }
+cd quickSSH || { echo; echo "Failed to enter directory."; exit 1; }
 
 # Make the script executable
 chmod +x "$SCRIPT_NAME"
 
 # Move the script to /usr/local/bin
 if ! sudo cp "$SCRIPT_NAME" "$INSTALL_PATH"; then
+    echo
     echo "Failed to copy script to $INSTALL_PATH."
     exit 1
 fi
@@ -23,5 +25,5 @@ fi
 # Clean up
 cd ..
 rm -rf quickSSH
-
+echo
 echo "Installation complete. You can now use the 'quickssh' command."
