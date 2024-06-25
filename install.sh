fbf008f6ee752c28b7c15ce09e3d7916b30c6d22
Bryce Rohloff <126307731+bmrolo@users.noreply.github.com>
2024-06-25 00:14:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ SCRIPT_NAME="quickssh.sh"
 INSTALL_PATH="/usr/local/bin/quickssh"
 
 # Clone the repository
-if ! git clone -q "$REPO_URL"; then
+if ! git clone --quiet "$REPO_URL"; then
     echo "Failed to clone repository."
     exit 1
 fi
