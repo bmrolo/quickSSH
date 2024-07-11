5ea3020282515e22a893a6eb20b3aaf114c484a9
Bryce Rohloff <126307731+bmrolo@users.noreply.github.com>
2024-07-11 14:39:56 -0400

    Added multi-worded support by using jq and the aws json output

--- quickssh.sh
+++ quickssh.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Check Dependencies
+if ! command -v fzf &> /dev/null; then
+    echo "fzf is not installed. Please install it first."
+    exit 1
+fi
+
+if ! command -v jq &> /dev/null; then
+    echo "jq is not installed. Please install it first."
+    exit 1
+fi
+
 echo -n "Enter Public IP (or press Enter to search with AWS CLI): "
 read ec2_ipaddress
 if [ -z "$ec2_ipaddress" ]; then
@@ -10,7 +21,8 @@ if [ -z "$ec2_ipaddress" ]; then
     fi
 
     # Get the list of EC2 instances
-    instances=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value | [0], InstanceId, KeyName, PublicIpAddress]' --output text)
+    instances=$(aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, KeyName, PublicIpAddress]' --output json)
+    num_instances=$(($(echo "$instances" | jq length)))
 
     # Check if there are any instances
     if [ -z "$instances" ]; then
@@ -20,27 +32,30 @@ if [ -z "$ec2_ipaddress" ]; then
 
     # Display the list of instances and prompt the user to select one
     echo "Available EC2 instances:"
-    instance_count=0
-    while read -r instance; do
-        instance_count=$((instance_count + 1))
-        instance_details=($instance)
-        printf "%2d: NAME: %s  ID: %s  KEY: %s  IP: %s\n" "$instance_count" "${instance_details[0]}" "${instance_details[1]}" "${instance_details[2]}" "${instance_details[3]}"
-    done <<< "$instances"
+    instance_iteration_count=1
+    for i in $(seq 1 $num_instances); do
+        instance_name=$(echo "$instances" | jq -r ".[$instance_iteration_count-1][0]")
+        instance_id=$(echo "$instances" | jq -r ".[$instance_iteration_count-1][1]")
+        instance_key=$(echo "$instances" | jq -r ".[$instance_iteration_count-1][2]")
+        instance_ip=$(echo "$instances" | jq -r ".[$instance_iteration_count-1][3]")
+        printf "%2d: NAME: %s  ID: %s  KEY: %s  IP: %s\n" "$instance_iteration_count" "$instance_name" "$instance_id" "$instance_key" "$instance_ip"
+        instance_iteration_count=$((instance_iteration_count + 1))
+    done
 
     echo -n "Enter the instance number you want to connect to: "
     read instance_number
 
     # Validate the user input
-    if ! [[ "$instance_number" =~ ^[0-9]+$ ]] || [ "$instance_number" -lt 1 ] || [ "$instance_number" -gt "$instance_count" ]; then
+    if ! [[ "$instance_number" =~ ^[0-9]+$ ]] || [ "$instance_number" -lt 1 ] || [ "$instance_number" -gt "$instance_iteration_count" ]; then
         echo "Invalid instance number selected."
         exit 1
     fi
 
     # Get the selected instance details
-    selected_instance=$(echo "$instances" | awk -v line="$instance_number" 'NR == line')
-    selected_instance_details=($selected_instance)
-    ec2_ipaddress="${selected_instance_details[3]}"
-    selected_key_name="${selected_instance_details[2]}"
+    selected_instance=$(echo "$instances" | jq -r ".[$instance_number-1]")
+    ec2_ipaddress=$(echo "$selected_instance" | jq -r ".[3]")
+    selected_key_name=$(echo "$selected_instance" | jq -r ".[2]")
+
 fi
 
 echo "Where is your SSH key located?"
@@ -65,7 +80,7 @@ elif [[ "$key_location" == "2" ]]; then
     get_ssh_key
 elif [[ "$key_location" == "3" ]]; then
     checkPath="$HOME"
-    cd $HOME
+    cd $checkPath
     get_ssh_key
 elif [[ "$key_location" == "4" ]]; then
     echo -n "Enter the custom directory path: "
